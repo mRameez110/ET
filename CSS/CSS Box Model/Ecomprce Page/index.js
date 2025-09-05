448c1766ff484703b9305775651906ce98997e0f
Rameez <114074269+mRameez110@users.noreply.github.com>
2025-09-05 22:23:12 +0500

    Add product name to cart array

--- CSS/CSS Box Model/Ecomprce Page/index.js
+++ CSS/CSS Box Model/Ecomprce Page/index.js
@@ -6,5 +6,6 @@ document.querySelectorAll(".add-to-cart").forEach((button) => {
   button.addEventListener("click", function () {
     const productName =
       this.previousElementSibling.previousElementSibling.innerText;
+    cart.push(productName);
   });
 });
