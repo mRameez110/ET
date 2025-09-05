98fc259ccb754b092a4e86bb946cc44cf4de8d49
Rameez <114074269+mRameez110@users.noreply.github.com>
2025-09-05 22:20:20 +0500

    Select all elements with class .add-to-cart

--- CSS/CSS Box Model/Ecomprce Page/index.js
+++ CSS/CSS Box Model/Ecomprce Page/index.js
@@ -1,3 +1,5 @@
 // JavaScript for adding products to the cart (optional feature)
 let cart = [];
 // Adding event listeners for each "Add to Cart" button
+
+document.querySelectorAll(".add-to-cart");
