ef2628684e3fe32e2ba3b57ff93e9725585fd1a7
Rameez <114074269+mRameez110@users.noreply.github.com>
2025-09-05 22:24:48 +0500

    Fix minor typos

--- CSS/CSS Box Model/Ecomprce Page/index.js
+++ CSS/CSS Box Model/Ecomprce Page/index.js
@@ -1,7 +1,7 @@
 // JavaScript for adding products to the cart (optional feature)
 let cart = [];
-// Adding event listeners for each "Add to Cart" button
 
+// Adding event listeners for each "Add to Cart" button
 document.querySelectorAll(".add-to-cart").forEach((button) => {
   button.addEventListener("click", function () {
     const productName =
