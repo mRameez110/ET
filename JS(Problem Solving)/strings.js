960002ffd63206e753f3cf3f2b8361544dadd98c
Rameez <temprary87@gmail.com>
2025-09-29 20:36:05 +0500

    Add problem statement for reverse string

---
+++ JS(Problem Solving)/strings.js
@@ -0,0 +1,7 @@
+// ✅ Phase 1: String Practice — Your Own Problems
+// 🔰 Level 0: Basic Manual Practice (No Built-ins Like split(), reverse(), etc.)
+// Here are 10 beginner-level string questions...
+
+// 🧩 1. Reverse a string manually
+// Input: "hello"
+// Output: "olleh"
