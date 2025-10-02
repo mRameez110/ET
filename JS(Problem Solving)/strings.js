2cfedd81ebae0aba88e715a2ee5e205a6c43afe3
Rameez <temprary87@gmail.com>
2025-10-02 19:35:13 +0500

    Run loop to copare the each character of string

--- JS(Problem Solving)/strings.js
+++ JS(Problem Solving)/strings.js
@@ -155,4 +155,9 @@
 
 const compStrFun = (str1, str2) => {
   if (str1.length != str2.length) return false;
+  for (let i = 0; i < str1.length; i++) {
+    if (str1[i] != str2[i]) {
+      return false;
+    }
+  }
 };
