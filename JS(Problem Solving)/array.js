914ace4e53dbce42b0bed941842745f01650a858
Rameez <temprary87@gmail.com>
2025-10-07 19:10:09 +0500

    Run for-loop for arr2 to push in mergedArr

--- JS(Problem Solving)/array.js
+++ JS(Problem Solving)/array.js
@@ -62,4 +62,7 @@ const mergedArrayFunc = (arr1, arr2) => {
   for (let i = 0; i < arr1.length; i++) {
     mergedArr.push(arr1[i]);
   }
+  for (let i = 0; i < arr2.length; i++) {
+    mergedArr.push(arr2[i]);
+  }
 };
