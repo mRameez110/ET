9262831b74189da77ee165bfa2067974f9a57184
Rameez <temprary87@gmail.com>
2025-10-09 19:23:34 +0500

    Add function example demonstrating use of any type

--- Type Script Learning/Basic ts learning/src/index.ts
+++ Type Script Learning/Basic ts learning/src/index.ts
@@ -6,7 +6,11 @@
 
 // let email = user.email; // in ts it error but valid in js
 
-let sales = 123_456_789;
-let course: string = "TypeScript Course";
-let isBeginner: boolean = true;
-let isPublished;
+// let sales = 123_456_789;
+// let course: string = "TypeScript Course";
+// let isBeginner: boolean = true;
+// let isPublished;
+
+function render(something: any) {
+  console.log(something);
+}
