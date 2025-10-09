1f082ea3dec53be1539575ee0c3fc5e7c6a005a9
Rameez <temprary87@gmail.com>
2025-10-09 19:21:55 +0500

    Initialize project with TypeScript basics and static typing example

---
+++ Type Script Learning/Basic ts learning/src/index.ts
@@ -0,0 +1,7 @@
+// --- > Why TypeScript <---
+
+// Type script provide static type checking
+
+// let user = { name: "rameez", age: 24 };
+
+// let email = user.email; // in ts it error but valid in js
