111623f556ecdc0a701208b87b226fb5dce63315
Rameez <temprary87@gmail.com>
2025-10-21 20:34:52 +0500

    Declare Array of type 'any'

--- Type Script Learning/TS by Dev_Gray/ts-series/src/main.ts
+++ Type Script Learning/TS by Dev_Gray/ts-series/src/main.ts
@@ -11,11 +11,13 @@
 // };
 // console.log(sum(5, "2"));
 
-let strArr = ["a", "b", "c"];
-let str_numArr = ["a", 2];
-let mixArr = ["a", 3, true];
+// let strArr = ["a", "b", "c"];
+// let str_numArr = ["a", 2];
+// let mixArr = ["a", 3, true];
 
 // strArr[0] = 5;
 // strArr.push(5);
-str_numArr[0] = 5; //   ----> No Error bcz type may be string or number
-str_numArr[1] = true; //   ----> Error: Type 'boolean' is not assignable to type 'number'.
+// str_numArr[0] = 5; //   ----> No Error bcz type may be string or number
+// str_numArr[1] = true; //   ----> Error: Type 'boolean' is not assignable to type 'number'.
+
+let anyTypeArr = [];
