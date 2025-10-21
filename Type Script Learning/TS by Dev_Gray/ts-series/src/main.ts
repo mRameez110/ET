06ae1da2ff3626e541c2fdaca1d7dc3e9ee22105
Rameez <temprary87@gmail.com>
2025-10-21 20:33:08 +0500

    Add type that is not in union is not allow

--- Type Script Learning/TS by Dev_Gray/ts-series/src/main.ts
+++ Type Script Learning/TS by Dev_Gray/ts-series/src/main.ts
@@ -15,6 +15,7 @@ let strArr = ["a", "b", "c"];
 let str_numArr = ["a", 2];
 let mixArr = ["a", 3, true];
 
-strArr[0] = 5;
-strArr.push(5);
+// strArr[0] = 5;
+// strArr.push(5);
 str_numArr[0] = 5; //   ----> No Error bcz type may be string or number
+str_numArr[1] = true; //   ----> Error: Type 'boolean' is not assignable to type 'number'.
