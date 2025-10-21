df56ec5ceaae744fb886b29d4129a74b834c6a99
Rameez <temprary87@gmail.com>
2025-10-21 18:46:42 +0500

    Set build as output directory from tsconfig.json configurations

---
+++ Type Script Learning/TS by Dev_Gray/ts-series/build/js/main.js
@@ -0,0 +1,16 @@
+"use strict";
+//   ----> Basic of TS
+// let a = 5;
+// let b = "2";
+// let c;
+// c = a / b;
+// console.log(c);
+// const sum = (a: number, b: string) => {
+//   return a + b;
+// };
+// console.log(sum(5, "2"));
+let strArr = ["a", "b", "c"];
+let str_numArr = ["a", 2];
+let mixArr = ["a", 3, true];
+// strArr[0] = 5;
+// strArr.push(5);
