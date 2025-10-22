f922344f361bc9620a3eb6b0501cf6492a820a24
Rameez <temprary87@gmail.com>
2025-10-22 19:42:36 +0500

    Tuple is more specific and strict than union type

--- Type Script Learning/TS by Dev_Gray/ts-series/src/main.ts
+++ Type Script Learning/TS by Dev_Gray/ts-series/src/main.ts
@@ -39,3 +39,5 @@ let unionTypeArrSimilarToMyTuple = ["rameez", 24, true];
 unionTypeArrSimilarToMyTuple = myTuple; // my tuple assigned to union type array
 // but union type array will not assign to my tuple
 myTuple = unionTypeArrSimilarToMyTuple; //   ----> Error: Type '(string | number | boolean)[]' is not assignable to type '[string, number, boolean]'. Target requires 3 element(s) but source may have fewer.ts(2322)
+
+// coclusion: tuple is more strict and specific than union type array
