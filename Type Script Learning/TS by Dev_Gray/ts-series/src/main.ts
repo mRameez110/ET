8283334520fdd70d488bf0677e58d2d4285917e0
Rameez <temprary87@gmail.com>
2025-10-27 20:54:12 +0500

    Test stringEcho

--- Type Script Learning/TS by Dev_Gray/ts-series/src/main.ts
+++ Type Script Learning/TS by Dev_Gray/ts-series/src/main.ts
@@ -138,5 +138,8 @@ console.log(john2);
 // }
 // console.log(createPair<string, number>("hello", 42));
 
+
 const stringEcho = (arg: string): string => arg;
 const echo = <T>(arg: T): T => arg;
+
+console.log(stringEcho("Rameez"));
