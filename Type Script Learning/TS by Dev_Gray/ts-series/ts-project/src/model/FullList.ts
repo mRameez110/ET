9efcababb4c97f9176b58fd939850092b449d013
Rameez <temprary87@gmail.com>
2025-11-03 20:59:55 +0500

    Add logic in load() to recreate ListItem objects

--- Type Script Learning/TS by Dev_Gray/ts-series/ts-project/src/model/FullList.ts
+++ Type Script Learning/TS by Dev_Gray/ts-series/ts-project/src/model/FullList.ts
@@ -14,14 +14,20 @@ export default class FullList implements List {
   get list(): ListItem[] {
     return this._list;
   }
-	load(): void {
-  const storedList: string | null = localStorage.getItem("myList");
-  if (typeof storedList !== "string") return;
-  const parsedList = JSON.parse(storedList);
-  parsedList.forEach(...);
-	}
-	save(): void {
-  localStorage.setItem("myList", JSON.stringify(this._list));
-}
-
+  load(): void {
+    const storedList: string | null = localStorage.getItem("myList");
+    if (typeof storedList !== "string") return;
+    const parsedList = JSON.parse(storedList);
+    parsedList.forEach((itemObj) => {
+      const newListItem = new ListItem(
+        itemObj._id,
+        itemObj._item,
+        itemObj._checked
+      );
+      FullList.instance.addItem(newListItem);
+    });
+  }
+  save(): void {
+    localStorage.setItem("myList", JSON.stringify(this._list));
+  }
 }
