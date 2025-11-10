781c4147b6360d42c6a8b0620891e136a45166c5
Rameez <temprary87@gmail.com>
2025-11-10 20:49:56 +0500

    Initialize main application function

--- Type Script Learning/TS by Dev_Gray/ts-series/ts-project/src/main.ts
+++ Type Script Learning/TS by Dev_Gray/ts-series/ts-project/src/main.ts
@@ -3,3 +3,8 @@ declare module '*.css';
 import FullList from "./model/FullList";
 import ListItem from "./model/ListItem";
 import ListTemplate from "./templates/ListTemplate";
+
+const initApp = (): void => {
+    const fullList = FullList.instance;
+    const template = ListTemplate.instance;
+}
