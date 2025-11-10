907f482b44b390537bccc397b6e01febfac1bcca
Rameez <temprary87@gmail.com>
2025-11-10 20:49:35 +0500

    Import core modules and styles

--- Type Script Learning/TS by Dev_Gray/ts-series/ts-project/src/main.ts
+++ Type Script Learning/TS by Dev_Gray/ts-series/ts-project/src/main.ts
@@ -1,1 +1,5 @@
-import "./css/style.css";
+// src/declarations.d.ts
+declare module '*.css';
+import FullList from "./model/FullList";
+import ListItem from "./model/ListItem";
+import ListTemplate from "./templates/ListTemplate";
