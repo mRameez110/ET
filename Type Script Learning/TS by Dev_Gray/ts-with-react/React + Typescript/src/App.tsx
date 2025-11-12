7716ddd0030d124378dc249e9ef8b2656344ece4
Rameez <temprary87@gmail.com>
2025-11-12 20:54:46 +0500

    Added App.tsx

---
+++ Type Script Learning/TS by Dev_Gray/ts-with-react/React + Typescript/src/App.tsx
@@ -0,0 +1,13 @@
+import Heading from "./components/Headings";
+import { Section } from "./components/Section";
+
+function App() {
+	return (
+		<>
+			<Heading title={"hello"} />
+			<Section title={"Different Title"}>This is my Section.</Section>
+		</>
+	);
+}
+
+export default App;
