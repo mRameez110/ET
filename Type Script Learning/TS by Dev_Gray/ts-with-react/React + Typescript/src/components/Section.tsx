377b94f0e01e2ff54441854c18693d3a213fb3fa
Rameez <temprary87@gmail.com>
2025-11-13 19:57:05 +0500

    Implemet Section functional component

--- Type Script Learning/TS by Dev_Gray/ts-with-react/React + Typescript/src/components/Section.tsx
+++ Type Script Learning/TS by Dev_Gray/ts-with-react/React + Typescript/src/components/Section.tsx
@@ -4,3 +4,12 @@ type SectionProps = {
   title?: string;
   children: ReactNode;
 };
+
+export const Section = ({
+  children,
+  title = "My Subheading",
+}: SectionProps) => {
+  return (
+    
+  );
+};
