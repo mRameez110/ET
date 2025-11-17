54d0c5b828f7f06313b7fdb65ffd621ad920b644
Rameez <temprary87@gmail.com>
2025-11-17 20:34:58 +0500

    Add input change handler

--- Type Script Learning/react-with-ts/src/components/Form.tsx
+++ Type Script Learning/react-with-ts/src/components/Form.tsx
@@ -4,4 +4,8 @@ interface ButtonProps {
   onClick: () => void;
 }
 
-const MyForm: React.FC<ButtonProps> = (props) => {};
+const MyForm: React.FC<ButtonProps> = (props) => {
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log("see input value", e.target.value);
+  };
+};
