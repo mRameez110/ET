b5c09cd6eb773b78ddc2e849726b6dd6d91c22c4
Rameez <temprary87@gmail.com>
2025-11-17 20:39:42 +0500

    Export component MyForm

--- Type Script Learning/react-with-ts/src/components/Form.tsx
+++ Type Script Learning/react-with-ts/src/components/Form.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+
 interface ButtonProps {
   text: string;
   onClick: () => void;
@@ -8,10 +9,13 @@ const MyForm: React.FC<ButtonProps> = (props) => {
   const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     console.log("see input value", e.target.value);
   };
+
   const handleSubmit = (e: any) => {
     e.preventDefault();
+
     console.log("Form Submitted event", e.target);
   };
+
   return (
     <>
       <h1>This is form Component</h1>
@@ -29,3 +33,5 @@ const MyForm: React.FC<ButtonProps> = (props) => {
     </>
   );
 };
+
+export default MyForm;
