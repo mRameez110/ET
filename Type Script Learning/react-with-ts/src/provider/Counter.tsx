cbf2e8955414ff16ac410823cfb0689095041c4a
Rameez <temprary87@gmail.com>
2025-11-18 20:42:28 +0500

    Define CounterContext value interface

--- Type Script Learning/react-with-ts/src/provider/Counter.tsx
+++ Type Script Learning/react-with-ts/src/provider/Counter.tsx
@@ -3,3 +3,7 @@ import React, { Children, createContext, useContext, useState } from "react";
 interface CounterProviderProps {
   children: React.ReactNode;
 }
+interface CounterContextValue {
+  value: number;
+  setCount: (num: number) => void;
+}
