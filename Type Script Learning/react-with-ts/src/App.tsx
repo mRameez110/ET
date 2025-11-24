f59840b1cc682eb61514064399e847756a153d00
Rameez <temprary87@gmail.com>
2025-11-24 20:25:22 +0500

    Import counterProvider

--- Type Script Learning/react-with-ts/src/App.tsx
+++ Type Script Learning/react-with-ts/src/App.tsx
@@ -3,3 +3,4 @@ import logo from "./logo.svg";
 // import "./App.css";
 import MyButton from "./components/Button";
 import MyForm from "./components/Form";
+import { CounterProvider } from "./provider/Counter";
