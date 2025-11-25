8c13bc94949521edd9b11ba0b4d9d1fd85ba927c
Rameez <temprary87@gmail.com>
2025-11-25 21:09:11 +0500

    Add gallows structure

--- Type Script Learning/hangman-react/src/HangmanDrawing.tsx
+++ Type Script Learning/hangman-react/src/HangmanDrawing.tsx
@@ -24,5 +24,14 @@ const LEFT_LEG = (...)
 
 export function HangmanDrawing({BODY_PARTS.slice(0, numberOfGuesses)}
 : HangmanDrawingProps) {
-  return <div style={{ position: "relative" }}></div>
+  return <div style={{ position: "relative" }}>
+    <div style={{ height: "50px", width: "10px", background: "black", position: "absolute", top: 0, right: 0 }} />
+
+<div style={{ height: "10px", width: "200px", background: "black", marginLeft: "120px" }} />
+
+<div style={{ height: "400px", width: "10px", background: "black", marginLeft: "120px" }} />
+
+<div style={{ height: "10px", width: "250px", background: "black" }} />
+
+  </div>
 }
