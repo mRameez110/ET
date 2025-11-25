fe10d11b0ce3fa428908e4866bf0fafb667629af
Rameez <temprary87@gmail.com>
2025-11-25 21:06:33 +0500

    Add HEAD element

--- Type Script Learning/hangman-react/src/HangmanDrawing.tsx
+++ Type Script Learning/hangman-react/src/HangmanDrawing.tsx
@@ -1,3 +1,18 @@
+const HEAD = (
+  <div
+    style={{
+      width: "50px",
+      height: "50px",
+      borderRadius: "100%",
+      border: "10px solid black",
+      position: "absolute",
+      top: "50px",
+      right: "-30px",
+    }}
+  />
+)
+
+
 export function HangmanDrawing({ numberOfGuesses }: HangmanDrawingProps) {
   return <div style={{ position: "relative" }}></div>
 }
