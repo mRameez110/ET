15773d7d1fb82358a203781f3abfa2985c2c1279
Rameez <temprary87@gmail.com>
2025-11-26 21:01:46 +0500

    Fix minor typos

--- Type Script Learning/hangman-react/src/HangmanWord.tsx
+++ Type Script Learning/hangman-react/src/HangmanWord.tsx
@@ -9,31 +9,33 @@ export function HangmanWord({
   wordToGuess,
   reveal = false,
 }: HangmanWordProps) {
-    return (
+  return (
     <div
       style={{
-          display: "flex",
-                gap: ".25em",
+        display: "flex",
+        gap: ".25em",
         fontSize: "6rem",
-          fontWeight: "bold",
-                textTransform: "uppercase",
+        fontWeight: "bold",
+        textTransform: "uppercase",
         fontFamily: "monospace",
       }}
-      >
-              {wordToGuess.split("").map((letter, index) => (
-                <span style={{ borderBottom: ".1em solid black" }} key={index}>
-                <span
-            style={{visibility:
+    >
+      {wordToGuess.split("").map((letter, index) => (
+        <span style={{ borderBottom: ".1em solid black" }} key={index}>
+          <span
+            style={{
+              visibility:
                 guessedLetters.includes(letter) || reveal
                   ? "visible"
-                        : "hidden",
-                            color:
+                  : "hidden",
+              color:
                 !guessedLetters.includes(letter) && reveal ? "red" : "black",
             }}
           >
             {letter}
           </span>
-
-
-
-
+        </span>
+      ))}
+    </div>
+  )
+}
