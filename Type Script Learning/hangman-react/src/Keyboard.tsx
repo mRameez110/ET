f8b209683364e393d5c9345e77f1f5b1d16254ac
Rameez <temprary87@gmail.com>
2025-11-28 21:12:34 +0500

    Fix minor typos

--- Type Script Learning/hangman-react/src/Keyboard.tsx
+++ Type Script Learning/hangman-react/src/Keyboard.tsx
@@ -1,8 +1,32 @@
 import styles from "./Keyboard.module.css"
 
 const KEYS = [
-  "a","b","c","d","e","f","g","h","i","j","k","l","m",
-  "n","o","p","q","r","s","t","u","v","w","x","y","z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
 ]
 
 type KeyboardProps = {
@@ -18,18 +42,22 @@ export function Keyboard({
   addGuessedLetter,
   disabled = false,
 }: KeyboardProps) {
-    return (
+  return (
     <div
       style={{
-          display: "grid",
-        >
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))",
+        gap: ".5rem",
+      }}
+    >
       {KEYS.map(key => {
         const isActive = activeLetters.includes(key)
         const isInactive = inactiveLetters.includes(key)
-         return (
+        return (
           <button
-             onClick={() => addGuessedLetter(key)}
-                       className={`${styles.btn} ${isActive ? styles.active : ""} ${isInactive ? styles.inactive : ""}`}
+            onClick={() => addGuessedLetter(key)}
+            className={`${styles.btn} ${isActive ? styles.active : ""} ${isInactive ? styles.inactive : ""
+              }`}
             disabled={isInactive || isActive || disabled}
             key={key}
           >
@@ -37,4 +65,6 @@ export function Keyboard({
           </button>
         )
       })}
-
+    </div>
+  )
+}
