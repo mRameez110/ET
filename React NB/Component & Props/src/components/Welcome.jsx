e22b445ce81b3e9c802a81b1d77a2b60fd502f33
Rameez <temprary87@gmail.com>
2025-12-04 20:41:35 +0500

    Commit 3: Add style to highlight unknown name in red

--- React NB/Component & Props/src/components/Welcome.jsx
+++ React NB/Component & Props/src/components/Welcome.jsx
@@ -1,2 +1,5 @@
 const Welcome = ({ name = "Unkown(Name is missing)", age }) => {
   const ageStyle = { color: age > 20 ? "blue" : "green" };
+  const unknownStyle = {
+    color: name === "Unkown(Name is missing)" ? "red" : "",
+  };
