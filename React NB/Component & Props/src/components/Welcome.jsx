a1822dd950abe8377efd15c15d12fc6fd69a9356
Rameez <temprary87@gmail.com>
2025-12-04 20:41:54 +0500

    Commit 4: Render greeting message with conditional styling

--- React NB/Component & Props/src/components/Welcome.jsx
+++ React NB/Component & Props/src/components/Welcome.jsx
@@ -2,4 +2,12 @@ const Welcome = ({ name = "Unkown(Name is missing)", age }) => {
   const ageStyle = { color: age > 20 ? "blue" : "green" };
   const unknownStyle = {
     color: name === "Unkown(Name is missing)" ? "red" : "",
+        return (
+    <h1 style={ageStyle}>
+      Welcome Dear <span style={unknownStyle}>{name}</span>. Your are {age}{" "}
+      years old.
+    </h1>
+  );
+
   };
+
