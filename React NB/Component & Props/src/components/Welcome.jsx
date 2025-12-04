5d8870d355711d48a8b6f25a6b46b6e26c8508fe
Rameez <temprary87@gmail.com>
2025-12-04 20:43:07 +0500

    Export the component

--- React NB/Component & Props/src/components/Welcome.jsx
+++ React NB/Component & Props/src/components/Welcome.jsx
@@ -2,12 +2,14 @@ const Welcome = ({ name = "Unkown(Name is missing)", age }) => {
   const ageStyle = { color: age > 20 ? "blue" : "green" };
   const unknownStyle = {
     color: name === "Unkown(Name is missing)" ? "red" : "",
-        return (
+  };
+
+  return (
     <h1 style={ageStyle}>
       Welcome Dear <span style={unknownStyle}>{name}</span>. Your are {age}{" "}
       years old.
     </h1>
   );
+};
 
-  };
-
+export default Welcome;
