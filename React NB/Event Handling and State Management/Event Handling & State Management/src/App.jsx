ec415d726877d15961aac31dcc6019750d4f6b64
Rameez <temprary87@gmail.com>
2025-12-08 20:25:47 +0500

    Issue to update instance by traditional way in React

--- React NB/Event Handling and State Management/Event Handling & State Management/src/App.jsx
+++ React NB/Event Handling and State Management/Event Handling & State Management/src/App.jsx
@@ -5,13 +5,26 @@ export default function App() {
     function OnClick(name) {
     console.log(name)
     alert("I m clicked by " +name);
+
+    // Issue with updating standard variable value in UI (without use State)
+  
+  let count = 5;
+  function increase(){
+    count  =count +1;
+    console.log(count)
+  }
   }
 
 return (
-    <div>
-      <h1>Welcome to React Event Handling</h1>
-      <button onClick={() => OnClick("Rameez")}>Click Me</button>
+    // <div>
+    //   <h1>Welcome to React Event Handling</h1>
+    //   <button onClick={() => OnClick("Rameez")}>Click Me</button>
+    // </div>
 
-    </div>
+    <div>
+      <h1>This is Counter Demo</h1>
+      <h4>count = {count}</h4>
+      <button onClick={increase}>increase</button>
+    </div>  
   );
 }
