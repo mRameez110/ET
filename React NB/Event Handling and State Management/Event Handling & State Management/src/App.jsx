d29e38815c785121e00ec47d5651d9de9e8b279b
Rameez <temprary87@gmail.com>
2025-12-08 20:29:28 +0500

    Fix minor issues

--- React NB/Event Handling and State Management/Event Handling & State Management/src/App.jsx
+++ React NB/Event Handling and State Management/Event Handling & State Management/src/App.jsx
@@ -1,45 +1,72 @@
+// Event Handling in JSX
+
 import React from 'react'
 import { useState } from 'react';
 
+
+
 export default function App() {
-    function OnClick(name) {
-    console.log(name)
-    alert("I m clicked by " +name);
 
-    // Issue with updating standard variable value in UI (without use State)
+  // function OnClick(name) {
+  //   console.log(name)
+
+  //   alert("I m clicked by " +name);
+  // }
+
+  // Issue with updating standard variable value in UI (without use State)
   
-  let count = 5;
-  function increase(){
-    count  =count +1;
-    console.log(count)
-  }
+  // let count = 5;
 
-    // Solution: use the useState hook
+  // function increase(){
+  //   count  =count +1;
+  //   console.log(count)
+  // }
 
-     let [increment, setIncrement] = useState(0);
+  // Solution: use the useState hook
+
+
+  let [increment, setIncrement] = useState(0);
+
+
   function increastWithUseStateHook(){
+
     // increment = increment + 1;
     setIncrement(++increment);
   }
 
-  }
 
-return (
-    // <div>
-    //   <h1>Welcome to React Event Handling</h1>
-    //   <button onClick={() => OnClick("Rameez")}>Click Me</button>
-    // </div>
 
+
+
+  return (
+    
+    // <>
+    // <div>Event handling in React</div>
+    // <button onClick={()=>OnClick('Rameez')}> Clicke ME</button>
+    // </>
+    
+
+    // Issue with updating standard variable value in UI (without use State)
+
+    // <>
     // <div>
     //   <h1>This is Counter Demo</h1>
     //   <h4>count = {count}</h4>
     //   <button onClick={increase}>increase</button>
-    // </div>  
-
-     <div>
+    // </div>    
+    // </> 
+    
+    
+    
+      <>
+    <div>
       <h1>This is Counter Demo</h1>
       <h4>count = {increment}</h4>
       <button onClick={increastWithUseStateHook}>increase</button>
-    </div>   
-  );
+    </div>    
+    </>
+
+  )
 }
+
+
