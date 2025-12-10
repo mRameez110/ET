e6f398f607f8204ec895340971f4edaa7a8b2d16
Rameez <temprary87@gmail.com>
2025-12-10 20:29:20 +0500

    Add increaseCount function to update marks array and log values

--- React NB/Event Handling and State Management/State Management Advance/src/App.jsx
+++ React NB/Event Handling and State Management/State Management Advance/src/App.jsx
@@ -5,4 +5,12 @@ export default function App() {
   let [count, setCount] = useState(0);
     let [marks, setMarks] = useState([1, 2, 3, 4]);
 
+      function increaseCount(index) {
+    marks[index] = marks[index] +1;
+    setMarks(marks);
+    console.log("marks array ", marks);
+    console.log(count);
+  }
+
+
 }
