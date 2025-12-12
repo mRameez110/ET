5fd04f10e347115d2aec0b3b07eedd4ae5680046
Rameez <temprary87@gmail.com>
2025-12-12 21:04:06 +0500

    Initialize input state with email and password fields

--- React NB/Forms in React/Form in React/src/App.jsx
+++ React NB/Forms in React/Form in React/src/App.jsx
@@ -2,4 +2,10 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
-function BasicExample() {}
+function BasicExample() {
+  const [input, setInput] = useState({
+  email:"",
+  password:""
+});
+
+}
