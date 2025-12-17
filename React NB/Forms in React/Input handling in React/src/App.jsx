20eb82a3e029de180b624f4abada1375318fd80e
Rameez <temprary87@gmail.com>
2025-12-17 23:13:46 +0500

    Fixed minor typos

--- React NB/Forms in React/Input handling in React/src/App.jsx
+++ React NB/Forms in React/Input handling in React/src/App.jsx
@@ -1,43 +1,53 @@
 import React from "react";
 
 function App() {
-    let [user, setUser] = React.useState({
+  let [user, setUser] = React.useState({
     name: "",
-    age: "0"
+    age:"0"
   });
+  
 
-    function onChangeUser(event) {
-    let { name, value } = event.target;
+  function onChangeUser(event) {
 
-    setUser({ ...user, [name]: value });
+   
+    let {name, value} = event.target;
+
+    setUser({...user, [name]   : event.target.value})
 
     console.log(event.target.name, event.target.value);
-  }
-
-      // if(name === "name"){
+    // if(name === "name"){
     //   setUser({...user, name:value})
     // }
+
     // else if(name === "age"){
     //   setUser({...user, age:value})
     // }
+    
+  }
 
+  // function onChangeAge(event) {
+  //   age = event.target.value;
+  //   setAge(age);
+  // }
 
+  return (
+    <div className="container m-3">
+      <h1>Input Object Understading in React </h1>
 
-  return <div>
-          <div>
+      <div>
         <h5>User Name: </h5>
         <input type="text" name="name" value={user.name} onChange={onChangeUser} />
         <button onClick={''}> sub</button>
         Welcome {user.name}
       </div>
+
       <div>
         <h5>User Age: </h5>
         <input type="number" name="age" value={user.age} onChange={onChangeUser} />
         {user.name} Age is {user.age} years
       </div>
-
-
-  </div>;
+    </div>
+  );
 }
 
 export default App;
