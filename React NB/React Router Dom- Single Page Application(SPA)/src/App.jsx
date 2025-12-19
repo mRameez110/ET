3e1e8e10c6e7d01b088026873e07ea67e0ec0179
Rameez <temprary87@gmail.com>
2025-12-19 23:27:37 +0500

    create App functional component

--- React NB/React Router Dom- Single Page Application(SPA)/src/App.jsx
+++ React NB/React Router Dom- Single Page Application(SPA)/src/App.jsx
@@ -3,3 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import MenuPage from "./pages/MenuPage";
 import ProductDetailsPage from "./pages/ProductDetailsPage";
+
+export default function App() {
+
+}
