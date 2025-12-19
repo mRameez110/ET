9f1a701aba93adad7549404a54f843f65da400cf
Rameez <temprary87@gmail.com>
2025-12-19 23:27:01 +0500

    import ProductDetailsPage

--- React NB/React Router Dom- Single Page Application(SPA)/src/App.jsx
+++ React NB/React Router Dom- Single Page Application(SPA)/src/App.jsx
@@ -2,3 +2,4 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
 import MenuPage from "./pages/MenuPage";
+import ProductDetailsPage from "./pages/ProductDetailsPage";
