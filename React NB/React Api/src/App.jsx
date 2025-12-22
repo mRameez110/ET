7f85169dbccc1bce17608b04065a4041dae8cb3b
Rameez <temprary87@gmail.com>
2025-12-22 23:46:59 +0500

    update todos state

--- React NB/React Api/src/App.jsx
+++ React NB/React Api/src/App.jsx
@@ -8,7 +8,11 @@ function App() {
 		const res = await fetch("https://jsonplaceholder.typicode.com/todos");
 		console.log("see response ", res);
 		const data = await res.json();
-		}
+		console.log("see response in json ", data);
+		setTodos(data);
+	};
+
+		
 
 
 
