fccff6b1a79d2e349f37cf0a4408af501d56d5b8
Rameez <temprary87@gmail.com>
2025-12-22 23:42:42 +0500

    define async function to load todos and set loading state

--- React NB/React Api/src/App.jsx
+++ React NB/React Api/src/App.jsx
@@ -3,6 +3,10 @@ import { useState } from "react";
 function App() {
 		const [todos, setTodos] = useState([]);
 		const [isloadClicked, setIsloadClicked] = useState(false);
+		const loadTodos = async () => {
+		setIsloadClicked(true);
+		}
+
 
 
 }
