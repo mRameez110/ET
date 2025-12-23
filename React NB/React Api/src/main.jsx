615df2b7fc6e080b18f189417fa17cc5c4be0e7d
Rameez <temprary87@gmail.com>
2025-12-23 23:27:10 +0500

    Import BrowserRouter for application routing

--- React NB/React Api/src/main.jsx
+++ React NB/React Api/src/main.jsx
@@ -2,3 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
