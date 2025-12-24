3c5fca5e8be88f238b09dabbda30e5b07e900499
Rameez <temprary87@gmail.com>
2025-12-24 23:33:27 +0500

    Export quizData for reuse across application

--- React NB/Quiz Practice/src/quizData.js
+++ React NB/Quiz Practice/src/quizData.js
@@ -21,4 +21,26 @@ const quizData = [
     //     answer: "LHR",
     // },
 
+        {
+        "id": 2,
+        "statement": "Capital of Punjab is ?",
+        "options": ["LHR", "ISB", "KRI", "PSH"],
+        "answer": "LHR",
+    },
+    {
+        "id": 3,
+        "statement": "Capital of Punjab is ?",
+        "options": ["LHR", "ISB", "KRI", "PSH"],
+        "answer": "LHR",
+    },
+    {
+        "id": 4,
+        "statement": "Capital of Punjab is ?",
+        "options": ["LHR", "ISB", "KRI", "PSH"],
+        "answer": "LHR",
+    },
+
+
 ];
+
+export default quizData;
