031a164714d2bd46bc27ba257eabab9c4d316647
Rameez <temprary87@gmail.com>
2025-12-26 23:09:31 +0500

    Initilize Staate for result

--- React NB/Quiz Practice/src/App.jsx
+++ React NB/Quiz Practice/src/App.jsx
@@ -3,6 +3,7 @@ import quizData from './quizData.json';
 function App() {
   
   const [selectedAnswers, setSelectedAnswers] = useState({});
+  const [result, setResult] = useState(null);
   return (
    <>
    </>
