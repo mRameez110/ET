684b76246f6ba93a1afdfd3d2c24120a62c53980
Rameez <temprary87@gmail.com>
2025-12-26 23:21:10 +0500

    fix minor typos and export App

--- React NB/Quiz Practice/src/App.jsx
+++ React NB/Quiz Practice/src/App.jsx
@@ -1,18 +1,18 @@
-import quizData from './quizData.json';
+import React, { useState } from "react";
+import quizData from "./quizData";
 
 function App() {
-  
   const [selectedAnswers, setSelectedAnswers] = useState({});
   const [results, setResults] = useState(null);
 
-    const handleAnswer = (questionId, selectedOption) => {
+  const handleAnswer = (questionId, selectedOption) => {
     setSelectedAnswers({
       ...selectedAnswers,
       [questionId]: selectedOption,
     });
   };
 
-    const handleSubmit = () => {
+  const handleSubmit = () => {
     let correctCount = 0;
 
     quizData.forEach((q) => {
@@ -22,18 +22,18 @@ function App() {
     });
 
     setResults(`You got ${correctCount} out of ${quizData.length} correct!`);
-    console.log("Answer object ", selectedAnswers)
-
   };
 
+  console.log("Answer object ", selectedAnswers)
+
   return (
-   <>
-    <h1 className="text-center">Quiz Practice</h1>
-      <div className="container p-3"></div>
-              {quizData.map((q) => (
+    <>
+      <h1 className="text-center">Quiz Practice</h1>
+      <div className="container p-3">
+        {quizData.map((q) => (
           <div className="card m-3" key={q.id}>
             <div className="card-header">{q.statement}</div>
-                        <ul className="list-group list-group-flush">
+            <ul className="list-group list-group-flush">
               {q.options.map((opt, optInd) => (
                 <li
                   className={`list-group-item ${
@@ -47,11 +47,17 @@ function App() {
                 </li>
               ))}
             </ul>
-            <button className="btn btn-primary mt-3" onClick={handleSubmit}>
+          </div>
+        ))}
+
+        <button className="btn btn-primary mt-3" onClick={handleSubmit}>
           Submit Quiz
         </button>
-        {results && <h3 className="mt-4">{results}</h3>}
 
-   </>
+        {results && <h3 className="mt-4">{results}</h3>}
+      </div>
+    </>
   );
 }
+
+export default App;
