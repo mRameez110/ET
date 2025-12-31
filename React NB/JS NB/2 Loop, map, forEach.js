40d099f7992f10e178369da876081dec8aad8860
Rameez <temprary87@gmail.com>
2025-12-31 22:23:45 +0500

    demonstrate that map returns a new array (even when undefined values)

--- React NB/JS NB/2 Loop, map, forEach.js
+++ React NB/JS NB/2 Loop, map, forEach.js
@@ -1,8 +1,11 @@
 // ES-6: map, filter , forEach
 
-let marks = [10, 20, 30, 40, 50];
-let arr = marks.map(x => {
-    if(x < 25)
-        return x + 5;
-});
-console.log(arr);
+// let marks = [10, 20, 30, 40, 50];
+// let arr = marks.map(x => {
+//     if(x < 25)
+//         return x + 5;
+// });
+// console.log(arr);
+
+let ans1 = array.map(x => console.log(x * x));
+console.log(ans1);
