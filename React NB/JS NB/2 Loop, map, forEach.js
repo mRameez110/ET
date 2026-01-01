68087a42c952a92126472b0e7be4198e075c0f2b
Rameez <temprary87@gmail.com>
2026-01-01 22:36:38 +0500

    show value-based iteration using forEach with index support

--- React NB/JS NB/2 Loop, map, forEach.js
+++ React NB/JS NB/2 Loop, map, forEach.js
@@ -18,6 +18,10 @@ let array = [10, 20, 30, 40, 50];
 let squareArr = array.map(ahmad => ahmad * ahmad);
 console.log(squareArr);
 
-let mappedArray = array.map(x => x * 1.10);
-console.log(mappedArray);
-console.log(array);
+// let mappedArray = array.map(x => x * 1.10);
+// console.log(mappedArray);
+// console.log(array);
+
+// value based (ES6) - forEach
+array.forEach((x, i) => console.log(x, i));
+array.forEach(x => console.log(x + 5));
