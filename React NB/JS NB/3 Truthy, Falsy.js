708ef4243209e17edde8b4ad2e8fe910a9449e07
Rameez <temprary87@gmail.com>
2026-01-02 23:09:49 +0500

    Truth/Falsy behavior across JS data types

--- React NB/JS NB/3 Truthy, Falsy.js
+++ React NB/JS NB/3 Truthy, Falsy.js
@@ -1,2 +1,5 @@
 // falsy values
 // 0, NaN, "", false, [].length, Object.values({}).length
+
+// data types: number, string, boolean, undefined, array, object
+// truthy and falsy values in each data type
