b2533b87427e6080b08f77a437c4d7f76840db99
Rameez <temprary87@gmail.com>
2026-01-09 23:45:40 +0500

    Fix formatting

--- React NB/JS NB/4 Operators.js
+++ React NB/JS NB/4 Operators.js
@@ -10,37 +10,73 @@
 // F || T = T
 // F || F = F
 
+
 // Truthy Expr || whatever = Truthy Expr
+
 // Falsy || Expr = Expr
 
 // let ans = "" || "adasdf";
 // console.log(ans);
 
+
 // (? :) if expression
+
+// let x = 500;
+// let y = 4000;
+// let z = 9000;
 
 // let max = x > y ? (x > z ? x : z) : ( y > z ? y : z);
 // console.log(max);
 
+// let max = x > y && x > z ? x : (y > z ? y : z);
+// console.log(max);
+
+
+
+// let x = 1000;
+// let y = 2000;
+
 // let max = x > y && x;
 // console.log(max);
+
+
+
+// let max = x > y ? x : y;
+// console.log(max);
+
+
+
+// let x = 100;
+// let y = 20;
+
+// let max;
 
 // if(x > y)
 //     max = x;
 // else
 //     max = y;
 
+// console.log(max);
+
+
+
 
 // let error = true;
 // let errorMessage = "Error in loading data";
 
+
 // if(error === true)
 //     console.log(errorMessage);
+
 
 // <div> error && errorMessage </div>
 
 
+
+
 // logical operators
 // &&, ||, !
+// && logical and
 
 // let ans = !"";
 // console.log(ans);
@@ -48,29 +84,43 @@
 // let x = false;
 // console.log(!!x);
 
+
 // let ans = !!(10 > 5);
 // console.log(ans);
 
+
+// short circuit operator
 // Truthy && expr = expr
 // Falsy Expr && whatever = Falsy
 
 // let ans = !!(9-8-1 && 10);
 // console.log(ans);
 
+
+
 // let ans = 109 > 50 && 10 + 2;
 // console.log(ans);
+
+
+
 
 // let ans = 10 > 5 && 20 < 100;
 // console.log(ans);
 
+
+
+// Comparison operators
 // <, <=, >, >=, ==, !=
 
+// == (value), === (value + type) strict
 // let ans = 100 === "100";
 // console.log(ans);
 
 
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators
 
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators
+// operators
 
 // Assignment operators
 // Comparison operators
@@ -85,17 +135,50 @@
 // Relational operators
 
 
+
+
+
+
+// Arithmetic operators
 // let x = 10;
 // x += 8; // x = x + 8;
 // console.log(x);
 
 
+
+// let x = 10;
+// let y = 20;
+
 // let ans = x++ + y;
+
 // console.log(ans);
 
-// x++; // x = x + 1;
-// ++x; // x = x + 1;
 
 
-let ans = x / y;
-console.log(ans);
+// let x = 10;
+// let y = 20;
+
+// // x++; // x = x + 1;
+
+// // ++x; // x = x + 1;
+
+// console.log(x);
+
+
+// let ans = ;
+// console.log(ans);
+
+
+
+
+// let x = "def";
+// let y = "abc";
+
+// let ans = x / y;
+// console.log(ans);
+
+// let x = "def";
+// let y = "abc";
+
+// let ans = x / y;
+// console.log(ans);
