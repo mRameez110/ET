a437dfb76f1e035fddec2d5a87502481475c36a4
Rameez <temprary87@gmail.com>
2026-01-13 23:25:44 +0500

    create a 2D array to demonstrate nested reference behavior

--- React NB/JS NB/5 Deep vs Shallow copy.js
+++ React NB/JS NB/5 Deep vs Shallow copy.js
@@ -1,3 +1,10 @@
 // deep copy and shallow copy
 // value type: number, string, boolean
 // reference types: array, object, function
+
+// 2D
+let array = [
+    [100, 2, 3],
+    [10, 20, 45],
+    [3, 56, 88],
+];
