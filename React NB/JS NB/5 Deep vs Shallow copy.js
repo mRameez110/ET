f495618092f950e3c3e0c0ca87c88b2148443c98
Rameez <temprary87@gmail.com>
2026-01-13 23:34:19 +0500

    create complex nested reference data structure

--- React NB/JS NB/5 Deep vs Shallow copy.js
+++ React NB/JS NB/5 Deep vs Shallow copy.js
@@ -3,17 +3,21 @@
 // reference types: array, object, function
 
 // 2D
-let array = [
-    [100, 2, 3],
-    [10, 20, 45],
-    [3, 56, 88],
-];
+// let array = [
+//     [100, 2, 3],
+//     [10, 20, 45],
+//     [3, 56, 88],
+// ];
 
 // let copyArray = array; // shallow copy 
 
-let copyArray = [];
-copyArray[0] = [...array[0]];
-copyArray[0][0] = 99;
+// let copyArray = [];
+// copyArray[0] = [...array[0]];
+// copyArray[0][0] = 99;
 
 // console.log(array);
 // console.log(copyArray);
+
+let products = [
+    { id: 1, title: "Samsung Mobile A1", price: 50000, colors: ["black", "white"] }
+];
