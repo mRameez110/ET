5616e4d2083334e45e70b282192a619ee947fb06
Rameez <temprary87@gmail.com>
2026-01-14 23:48:47 +0500

    shallow copy array but inner arrays still referenced

--- React NB/JS NB/5 Deep vs Shallow copy.js
+++ React NB/JS NB/5 Deep vs Shallow copy.js
@@ -3,11 +3,11 @@
 // reference types: array, object, function
 
 // 2D
-// let array = [
-//     [100, 2, 3],
-//     [10, 20, 45],
-//     [3, 56, 88],
-// ];
+let array = [
+    [100, 2, 3],
+    [10, 20, 45],
+    [3, 56, 88],
+];
 
 // let copyArray = array; // shallow copy 
 
@@ -18,12 +18,14 @@
 // console.log(array);
 // console.log(copyArray);
 
-let products = [
-    { id: 1, title: "Samsung Mobile A1", price: 50000, colors: ["black", "white"] }
-];
+// let products = [
+//     { id: 1, title: "Samsung Mobile A1", price: 50000, colors: ["black", "white"] }
+// ];
 
-let copyProducts = products;  // deep copy ?
+// let copyProducts = products;  // deep copy ?
 
-copyProducts[0].price = 40000;
+// copyProducts[0].price = 40000;
 
-console.log(products);
+// console.log(products);
+
+let array2 = [...array];
