611fe7ef8d524b8e9f78b2649ed531ceec244a5d
Rameez <temprary87@gmail.com>
2026-01-15 23:18:18 +0500

    define object as product1 to demonstrate deep copy

--- React NB/JS NB/5 Deep vs Shallow copy.js
+++ React NB/JS NB/5 Deep vs Shallow copy.js
@@ -35,20 +35,27 @@ let array = [
 // console.log(array2);
 
 // deep copy ... 1 level ref data copy
+// let product1 = {
+//     id: 1,
+//     title: "Mobile",
+//     price: 50000,
+//     colors: ["black", "white"]
+// };
+
+// let product2 = {...product1};
+// product2.colors = [...product1.colors];
+
+// product2.colors[1] = "red";
+// console.log(product1);
+// console.log(product2);
+
+// product1.colors.push("green");
+// console.log(product1);
+// console.log(product2);
+
+// deep copy (...)
 let product1 = {
     id: 1,
     title: "Mobile",
-    price: 50000,
-    colors: ["black", "white"]
-};
-
-let product2 = {...product1};
-product2.colors = [...product1.colors];
-
-product2.colors[1] = "red";
-console.log(product1);
-console.log(product2);
-
-product1.colors.push("green");
-console.log(product1);
-console.log(product2);
+    price: 50000
+};  
