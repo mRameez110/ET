ffbcf02ee2e92745085280cdeb4adc8a180699c7
Rameez <temprary87@gmail.com>
2026-01-19 23:54:31 +0500

    demonstrating function scope

--- React NB/JS NB/6 Arrow Functions.js
+++ React NB/JS NB/6 Arrow Functions.js
@@ -1,7 +1,14 @@
-let x = [10, 20, 30];
-let y = [23, 445, 55];
-x = y;
-console.log(x);
+// let x = [10, 20, 30];
+// let y = [23, 445, 55];
+// x = y;
+// console.log(x);
 
 // scope and life time of variable
 // local vs global
+
+let x = 10;
+function f(i){
+     x = x + i;
+    console.log(x);
+    let x = 90;
+}
